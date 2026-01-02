c7b5fad285ffce0abd843754444d9a047ede424a
Anas Shafiq <anasshafiq666@gmail.com>
2026-01-02 22:21:35 +0500

    Clear Firebase config values
    Updated Firebase configuration to use empty strings.

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,12 +6,12 @@ import { enableIndexedDbPersistence, getFirestore } from 'firebase/firestore';
 // Note: These values are not "secret" by themselves, but hardcoding can make it easy
 // to accidentally commit project config to a public repo.
 const firebaseConfig = {
-  apiKey: 
-  authDomain: "fitness-challenge-tracke-9bafa.firebaseapp.com",
-  projectId: "fitness-challenge-tracke-9bafa",
-  storageBucket: "fitness-challenge-tracke-9bafa.firebasestorage.app",
-  messagingSenderId: "497755064584",
-  appId: "1:497755064584:web:d7c7cc1dc9373cea4704bd",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId:"",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
